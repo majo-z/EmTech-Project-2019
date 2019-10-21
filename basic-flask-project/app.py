74a8a309e5a26114da2b99a39d1233dc4b76a0c8
majo-z <g00340481@gmit.ie>
2019-10-21 10:38:57 +0100

    added another example to flask application

--- basic-flask-project/app.py
+++ basic-flask-project/app.py
@@ -2,13 +2,19 @@
 # https://abndistro.com/post/2019/01/20/using-flask-to-deploy-predictive-models/
 
 # app.py
-from flask import Flask
+from flask import Flask, escape, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello world!\n'
+    return 'Hello world 1\n'
+
+# adapted from https://palletsprojects.com/p/flask/
+@app.route('/hello')
+def hello():
+    name = request.args.get("name", "World 2")
+    return f'Hello, {escape(name)}!'
 
 if __name__ == '__main__':
     app.run()
